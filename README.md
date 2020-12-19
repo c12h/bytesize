9916063f7573d9e37c72d4a6b833d0de20c1cf30
Chris Chittleborough <cchittleborough@cluemail.com>
2020-12-19 20:15:20 +1100

    Punt all verbs other than %v and pad %#v correctly

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bytyesize
-Go package for easy output of byte counts as "1KiB", "3.17MB", "1.022MiB",  etc
+# bytesize
+Go package for easy output of byte counts as "1KiB", "3.17MB", "-1.022MiB",  etc
--- bytesize.go
+++ bytesize.go
@@ -1,18 +1,23 @@
-// Package bytesize provides type ByteSize, a number of bytes, which prints as
-// "1B", "2KiB", "3.45MiB", "67.8GiB", "987TiB", "1023EiB" etc.
+// Package bytesize lets you output "17Kib", "3.45MiB", "67.8GiB" etc when
+// printing numbers of byte.
 //
-// ByteSize is derived from int64, so int64(x) returns the underlying value of x.
+// It exports type ByteSize, which is a wrapper for int64.  Therefore (1)
+// ByteSize values can be negative, and (2) int64(x) returns the underlying
+// value of x.
 //
-// Note that ByteSize values may be negative.
+// When printing a ByteSize value with a format string, %#v generates what
+// fmt.Sprintf("ByteSize(%d)", int64(value)) would.  Using %v (without a '#'
+// flag) generates strings like "1B", "2KiB", "3.45MiB", "67.8GiB", "987TiB",
+// "1023EiB" etc.  In either case, the other conversion flags are ignored,
+// except that '-' and a width specifier have the usual effect.
 //
-// When printing a ByteSize value with a format string, the %s and %v verbs
-// print the value with 1 to 4 decimal digits, possibly including a decimal
-// point, followed by "B", "KiB", "MiB", "GiB", "TiB", "PiB" or "EiB" as
-// appropriate.  All other verbs, notably %d, act as though applied to the
-// underlying number.
+// If any other verb is used, the effect is the same as on the underlying value.
+// Note that fmt.Print(), fmt.Println() etc use %v on every operand.
 //
-// Specifying a precision in a format specifier does not affect how many digits
-// are output. Instead, a precision can select shorter forms of the suffixes:
+// When %v is used without the hash flag, the output is up to 4 decimal digits,
+// perhaps with a decimal point between two of those digits.  In this case,
+// specifying a precision in a format specifier does not affect how many digits
+// are output.  Instead, a precision can select shorter forms of the suffixes:
 //  Precision:	Suffixes:-
 //	.0	"",    "K",   "M",   "G",   "T",   "P",   "E"
 //	.1	"B",   "K",   "M",   "G",   "T",   "P",   "E"
@@ -20,14 +25,12 @@
 //	.3	"B",   "KiB", "MiB", "GiB", "TiB", "PiB", "EiB"
 // Other precision values are treated as .3, giving the default suffixes.
 //
-// ByteSize has a String() method, which always uses the default suffixes.
+// ByteSize has a String() method, which always uses the default suffixes.  For
+// completeness, ByteSize also has a GoString method, which is equivalent to
+// formatting the value with "%#v".
 //
-// For completeness, ByteSize also has a GoString method, which has the same
-// effect as fmt.Sprintf("ByteSize(%d)", int64(value)).
-//
-// This package exports only one directly-visible name, ByteSize.
-//
-// When using this package, you may want to define a type alias, like this:
+// This package exports only one directly-visible name, ByteSize.  When using
+// this package, you may want to define a type alias, like this:
 //	type ByteSize = bytesize.ByteSize
 // so you can write "ByteSize" instead of "bytesize.ByteSize".
 //
@@ -47,13 +50,13 @@ import (
 //
 type ByteSize int64
 
-// The String() method returns what formatting the value with "%.3v" would:
-// strings like "0B", ..., "1023B", "1KiB", ..., "1.01Kib", "-23.4MiB", "340GiB"
-// etc.
+// The String() method returns what formatting the value with "%v" (or "%.3v")
+// would: strings like "0B", ..., "1023B", "1KiB", ..., "1.01Kib", "-23.4MiB",
+// "340GiB" etc.
 //
 // This method makes ByteSize satisfy the fmt.Stringer interface.  (Because
-// ByteSize also satisfies the fmt.Formatter interface, fmt will use .Format(…)
-// rather than this method when printing ByteSize values.)
+// ByteSize also satisfies the fmt.Formatter interface, printing ByteSize values
+// will use .Format(…) rather than this method.)
 //
 func (n ByteSize) String() string {
 	return string(formatByteSize(int64(n), 3))
@@ -63,8 +66,8 @@ func (n ByteSize) String() string {
 // with "ByteSize(%d)".
 //
 // This method makes ByteSize satisfy the fmt.GoStringer interface.  (Because
-// ByteSize also satisfies the fmt.Formatter interface, fmt will use .Format(…)
-// rather than this method when printing ByteSize values.)
+// ByteSize also satisfies the fmt.Formatter interface, fmt will print ByteSize
+// values using .Format(…), not this method.)
 //
 func (n ByteSize) GoString() string {
 	return fmt.Sprintf("ByteSize(%d)", int64(n))
@@ -73,38 +76,36 @@ func (n ByteSize) GoString() string {
 // The Format method makes ByteSize satisfy the fmt.Formatter interface, and thus
 // controls how ByteSize values are printed.
 //
-// For verbs other than %s and %v, it simply applies the same conversion specifier
-// to the underlying int64 value.
+// For verbs other than %v, it simply applies the same conversion specifier to
+// the underlying int64 value.
 //
 // For %#v, it outputs fmt.Sprintf("ByteSize(%d)", int64(n)).
 //
-// For %s and %v without "#", it outputs a decimal number with 3 or 4
-// significant digits followed by the appropriate suffix.  The output may or may
-// not have a decimal point.  As mentioned in the package documentation, a
-// precision specifier format verb affects which suffixes are used, not how many
-// digits are output.
+// For %v without "#", it outputs a decimal number with 3 or 4 significant
+// digits followed by the appropriate suffix.  The output may or may not have a
+// decimal point.  As mentioned in the package documentation, a precision
+// specifier format verb affects which suffixes are used, not how many digits
+// are output.
 //
 func (b ByteSize) Format(f fmt.State, verb rune) {
 	nBytes := int64(b)
-	if verb == 'v' {
-		if f.Flag('#') {
-			fmt.Fprintf(f, "ByteSize(%d)", nBytes)
-			return
-		}
-		// else use formatByteSize()
-	} else if verb != 's' {
+	if verb != 'v' {
 		fmt.Fprintf(f, equivalentFormat(f, verb), nBytes)
 		return
 	}
 
-	prec, specified := f.Precision()
-	if !specified || prec > 3 {
-		prec = 3
-	} else if prec < 0 {
-		prec = 0
+	var output []byte
+	if f.Flag('#') {
+		output = []byte(fmt.Sprintf("ByteSize(%d)", nBytes))
+	} else {
+		prec, specified := f.Precision()
+		if !specified || prec > 3 {
+			prec = 3
+		} else if prec < 0 { // ‘cannot happen’, but cope anyhow
+			prec = 0
+		}
+		output = formatByteSize(nBytes, prec)
 	}
-
-	output := formatByteSize(nBytes, prec)
 
 	width, haveWidth := f.Width()
 	if !haveWidth {
@@ -138,10 +139,13 @@ func equivalentFormat(f fmt.State, verb rune) string {
 		formatString += "."
 		formatString += strconv.FormatInt(int64(p), 10)
 	}
-	return formatString + string(verb)
+	formatString += string(verb)
+	return formatString
 }
 
 // formatByteSize does the hard work for this package.
+//
+// Note that it returns a byte slice, not a string.
 func formatByteSize(value int64, prec int) []byte {
 	const suffix1 = "!KMGTPE"
 	const _1 = int64(1)
